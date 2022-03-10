1adba2bcc06251852dec9e84cd351bed57f4ee0f
Ayush Bindlish <ayushbindlish96@gmail.com>
2022-03-10 23:07:35 +0530

    freestanding rust binary

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Script to build free standing binary
-cargo rustc -- -C link-arg=-nostartfiles
+cargo rustc -- -C link-arg=-nostartfiles # compile for host system
+# cargo build --target thumbv7em-none-eabihf # compile for bare metal target
+
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 // To write an operating system kernel, we need code that does not depend on and
 // operating system.
-// Build command "cargo rustc -- -C link-arg=-nostartfiles"
 
-
-// Disable linking of the standard library
-#![no_std]
-
-#![no_main]
+// Disable automatic linking of the standard library
+#![no_std] // do not link the rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
+// panic_handler attribute defines the function
+// that the compiler invokes should a panic occur
+#[panic_handler] /// this function is called on panic
 fn panic(_info: &PanicInfo) -> ! {
+    // returns "never" type using "!" i.e. it does not return
+    // marked as diverging function by returning !
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // do not mangle the name of this function
 pub extern "C" fn _start() -> ! {
     loop {}
 }
